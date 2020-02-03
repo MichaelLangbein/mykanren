32e0ab3327803dee0c1c79e6566ef87646acea16
Langbein <Michael.Langbein@dlr.de>
2020-02-03 19:05:00 +0100

    added subtraction

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,17 @@
 const chai = require('chai');
 const expect = chai.expect;
 const add = require('../src/main').add;
+const subtract = require('../src/main').subtract;
 
 
 describe('testing main.js', () => {
     it('adding works as expected', () => {
         const sum = add(1, 2);
         expect(sum).to.equal(3);
+    });
+
+    it('subtraction works as expected', () => {
+        const difference = subtract(3, 2);
+        expect(difference).to.equal(1);
     });
 });
